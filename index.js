cd6d91da8272a5862adc2eab36b5ecbb7cd7c411
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-06 10:24:05 -0300

    exercício passado em sala

---
+++ index.js
@@ -0,0 +1,36 @@
+function vogais() {
+    document.getElementById("bloco1").innerHTML="A";
+    var obj1=document.getElementById("bloco1");
+    obj1.className="azul";
+
+    document.getElementById("bloco2").innerHTML = "B";
+    var obj2 = document.getElementById("bloco2");
+    obj2.className = "vermelho";
+
+    document.getElementById("bloco3").innerHTML = "C";
+    var obj3 = document.getElementById("bloco3");
+    obj3.className = "verde";
+
+    document.getElementById("bloco4").innerHTML = "D";
+    var obj4 = document.getElementById("bloco4");
+    obj4.className = "amarelo";
+
+}
+
+function numRomanos() {
+  document.getElementById("bloco1").innerHTML = "I";
+  var obj1 = document.getElementById("bloco1");
+  obj1.className = "vermelho";
+
+  document.getElementById("bloco2").innerHTML = "II";
+  var obj2 = document.getElementById("bloco2");
+  obj2.className = "verde";
+
+  document.getElementById("bloco3").innerHTML = "III";
+  var obj3 = document.getElementById("bloco3");
+  obj3.className = "amarelo";
+
+  document.getElementById("bloco4").innerHTML = "IV";
+  var obj4 = document.getElementById("bloco4");
+  obj4.className = "azul";
+}
