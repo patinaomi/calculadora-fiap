798ef2717171aeeca432f1bc0b64150a9c9eb734
Patricia Naomi <patricianaomiyama@gmail.com>
2023-12-14 19:18:39 -0300

    alterações

--- assets/index.js
+++ assets/index.js
@@ -1,36 +1,70 @@
-function vogais() {
-    document.getElementById("bloco1").innerHTML="A";
-    var obj1=document.getElementById("bloco1");
-    obj1.className="azul";
+let notaFinal;
 
-    document.getElementById("bloco2").innerHTML = "B";
-    var obj2 = document.getElementById("bloco2");
-    obj2.className = "vermelho";
-
-    document.getElementById("bloco3").innerHTML = "C";
-    var obj3 = document.getElementById("bloco3");
-    obj3.className = "verde";
-
-    document.getElementById("bloco4").innerHTML = "D";
-    var obj4 = document.getElementById("bloco4");
-    obj4.className = "amarelo";
-
+function validaNota(nota, maximo, elemento) {
+    if (nota < 0 || nota > maximo) {
+        alert(`Valor Inválido! As notas devem estar entre 0 e ${maximo}.`);
+        elemento.value = '';
+        elemento.focus();
+        return false;
+    }
+    return true;
 }
 
-function numRomanos() {
-  document.getElementById("bloco1").innerHTML = "I";
-  var obj1 = document.getElementById("bloco1");
-  obj1.className = "vermelho";
+function notasCheckpoint(cp1, cp2, cp3) {
+    let elementos = [document.getElementById('cp1-1'), document.getElementById('cp2-1'), document.getElementById('cp3-1')];
 
-  document.getElementById("bloco2").innerHTML = "II";
-  var obj2 = document.getElementById("bloco2");
-  obj2.className = "verde";
+    if (!validaNota(cp1, 10, elementos[0]) || !validaNota(cp2, 10, elementos[1]) || !validaNota(cp3, 10, elementos[2])) {
+        return null;
+    }
 
-  document.getElementById("bloco3").innerHTML = "III";
-  var obj3 = document.getElementById("bloco3");
-  obj3.className = "amarelo";
+    let notas = [cp1, cp2, cp3];
+    notas.sort((a, b) => b - a);
 
-  document.getElementById("bloco4").innerHTML = "IV";
-  var obj4 = document.getElementById("bloco4");
-  obj4.className = "azul";
+    return [notas[0], notas[1]];
 }
+
+function calculaSprintCp() {
+    let cp1Elem = document.getElementById('cp1-1');
+    let cp2Elem = document.getElementById('cp2-1');
+    let cp3Elem = document.getElementById('cp3-1');
+
+    let cp1 = parseFloat(cp1Elem.value);
+    let cp2 = parseFloat(cp2Elem.value);
+    let cp3 = parseFloat(cp3Elem.value);
+
+    let sprint1Elem = document.getElementById('sp1-1');
+    let sprint2Elem = document.getElementById('sp2-1');
+    let globalElem = document.getElementById('gs-1');
+
+    let sprint1 = parseFloat(sprint1Elem.value);
+    let sprint2 = parseFloat(sprint2Elem.value);
+    let global = parseFloat(globalElem.value);
+
+    if (!validaNota(sprint1, 100, sprint1Elem) || !validaNota(sprint2, 100, sprint2Elem) || !validaNota(global, 100, globalElem)) {
+        return;
+    }
+
+    let [nota1, nota2] = notasCheckpoint(cp1, cp2, cp3);
+    if (nota1 === null || nota2 === null) {
+        return;
+    }
+
+    nota1 *= 10;
+    nota2 *= 10;
+    let notaParcial = ((nota1 + nota2 + sprint1 + sprint2) / 4) * 0.4;
+
+    notaFinal = notaParcial + (global * 0.6);
+}
+
+function calcularMediaSemestral() {
+    if (notaFinal !== undefined) {
+        document.getElementById('resultado-1').textContent = notaFinal.toFixed(2);
+    }
+}
+
+function calcularMedia() {
+    calculaSprintCp();
+    calcularMediaSemestral();
+}
+
+document.querySelector('.botao-media').addEventListener('click', calcularMedia);
